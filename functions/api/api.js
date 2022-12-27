d078b86a48ece51405bb9c958b83b8d07c7b3080
TiffanyStPierre <stpierre.tiffany@gmail.com>
2022-12-27 13:33:06 -0700

    Created functions using event listeners to get tv or movie genres from API and display in dropdown menu

--- functions/api/api.js
+++ functions/api/api.js
@@ -24,19 +24,6 @@ const handler = async (event) => {
         body: JSON.stringify({status, statusText, headers, data})
       }
     }
-
-  /*try {
-    const subject = event.queryStringParameters.name || 'World'
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
-    }
-  } catch (error) {
-    return { statusCode: 500, body: error.toString() }
-  }*/
 }
 
 module.exports = { handler }
--- script.js
+++ script.js
@@ -1,5 +1,64 @@
 
-const media = 'movie';
+const movieButton = document.getElementById('movie-button');
+const tvButton = document.getElementById('tv-button');
 
-fetch(`/.netlify/functions/api?media=${media}`)
-    .then(res => {console.log(res.json())});
+
+/* Populate genre selector dropdown list for movie or tv genres*/
+
+const populateGenreSelector = (genres) => {
+    const select = document.getElementById('genres')
+
+    while (select.hasChildNodes()) {
+        select.removeChild(select.firstChild);
+    }
+
+    for (const genre of genres) {
+        let option = document.createElement("option");
+        option.value = genre.id;
+        option.text = genre.name;
+        select.appendChild(option);
+    }
+};
+
+/* Retrieve movie genres with API call*/
+
+const getMovieGenres = async() => {
+    
+    let media = 'movie';
+
+    try {
+        const response = await fetch(`/.netlify/functions/api?media=${media}`);
+            if (response.ok) {
+                const jsonResponse = await response.json();
+                const genres = jsonResponse.genres;
+                populateGenreSelector(genres);
+            }
+
+    } catch (e) {
+        console.log(e);
+    }
+};
+
+/* Retrieve TV genres with API call*/
+
+const getTvGenres = async() => {
+    
+    let media = 'tv';
+
+    try {
+        const response = await fetch(`/.netlify/functions/api?media=${media}`);
+            if (response.ok) {
+                const jsonResponse = await response.json();
+                const genres = jsonResponse.genres;
+                populateGenreSelector(genres);
+            }
+
+    } catch (e) {
+        console.log(e);
+    }
+};
+
+/*Event listeners for Movie & TV Show buttons for genre retrieval using API*/
+
+movieButton.addEventListener('click', getMovieGenres);
+tvButton.addEventListener('click', getTvGenres);
